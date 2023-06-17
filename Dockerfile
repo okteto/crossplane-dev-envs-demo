f95dfdb63cbfe8ab42efc053698e6343a8e523c8
Ramiro Berrelleza <ramiro@okteto.com>
2023-06-16 22:43:13 -0700

    create all infra with a composition

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM golang:buster
-
-WORKDIR /app
-ADD . .
-RUN go build -o /usr/local/bin/postgres-app
-
-EXPOSE 3000
-CMD ["/usr/local/bin/postgres-app"]
--- demo.sh
+++ demo.sh
@@ -12,6 +12,9 @@ helm install crossplane \
     --namespace crossplane-system \
     --create-namespace
 
+### Install Schema Hero
+HELM_EXPERIMENTAL_OCI=1 helm upgrade -i --wait --create-namespace -n schemahero schemahero oci://ghcr.io/schemahero/helm/schemahero
+
 ### Install Crossplane Provider AWS
 cat <<EOF | kubectl apply -f -
 apiVersion: pkg.crossplane.io/v1
@@ -48,37 +51,33 @@ EOF
 ### Apply the YAMLs for the Composite Resources
 kubectl apply -f packages
 
+
+
 ## TALK
 
 # CL: What do you need to develop your application?
-# DD: I need to bring up all the serivces that my application depends on. That would be a Postgres database and the go server in this case.
-
-# CL: How do you bring up the Postgres database?
-# DD: I would use the AWS console to create a RDS instance. But that is complicated and I don't have AWS knowledge and don't want to depend on you each time. I would rather have a simple way to bring up the database.
+# DD: I need to bring up all the services that my application depends on. That would be an S3 bucket, an SQS queue, a dynamo DB, a go server for batch processing, a fastAPI server, and a nodeJS frontend
+# CL: Whoa. That's a lot.  How do you bring up all those things?
+# DD: I would use the AWS console to create the S3 bucket, then there's an option somewhere there for SQS, and... I don't know :cry: Is complicated and I don't have AWS knowledge and don't want to depend on you each time. I would rather have a simple way to bring up the databases.
+# CL: Argh..that's too much work. No way that you can figure all that on your own. There's a better way
 
 kubectl create namespace dev
 
-kubectl --namespace dev apply \
-    --filename claim.yaml
+kubectl --namespace dev apply --filename claim.yaml
 
-kubectl --namespace dev \
-    get sqlclaims
+kubectl --namespace dev get tacoshop-database
 
 # CL: What else do you need?
-# DD: I need to be able to access that database server
+# DD: I need to be able to access the services
 
-kubectl --namespace dev \
-    get secrets
+kubectl --namespace dev get secrets
 
-./examples/sql/schemahero-secret.sh dev
+kubectl get queues.sqs.aws.upbound.io
 
-kubectl get databases.postgresql.sql.crossplane.io
-
-# Open pgAdmin and show that the DB was created
+# Open AWS and show that the SQS queue was created as an example
 
 # CL: What else do you need?
-# DD: I need my application up and running with the table created
+# DD: I need all those services I told you about up and running
 
 okteto context use
-
 okteto up --namespace dev
